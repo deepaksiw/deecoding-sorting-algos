2df5eb8e41ea8e02161300e353fe8bbacea7faa6
Deepak Pandey <deepaksiw@gmail.com>
2021-10-31 00:21:34 +0530

    Intial project version

--- src/com/deecoding/algo/sorting/Driver.java
+++ src/com/deecoding/algo/sorting/Driver.java
@@ -2,6 +2,7 @@ package com.deecoding.algo.sorting;
 
 public class Driver {
 	public static void main(String[] args) {
-		System.out.println("This id driver class...");
+		System.out.println("This is driver class..");
+		System.out.println("This may also act as Driver class for all the sortign algorithm..");
 	}
 }
